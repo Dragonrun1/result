66df100e57542ebe743448470693457bb6afcf4e
Michael Cummings <mgcummings@yahoo.com>
2025-07-01 10:39:13 -0700

    Reformat `result.cpp` test cases and add missing include for improved code clarity and structure.

--- test/result.cpp
+++ test/result.cpp
@@ -2,10 +2,11 @@
 // Copyright (c) 2025 Michael Cummings
 // SPDX-License-Identifier: MIT
 
+#include "../include/cpp_result/result.hpp"
+
 #include <iostream>
 #include <string>
 
-#include "../include/cpp_result/result.hpp"
 #include <catch2/catch_test_macros.hpp>
 
 using namespace result;
@@ -54,8 +55,11 @@ TEST_CASE("Result copy/move", "[result]") {
         {
             {
                 auto result1 = Result<std::string, std::string>(
-                        Ok(std::string("Hello there. This is a long string to "
-                                       "require heap allocation")));
+                    Ok(std::string(
+                        "Hello there. This is a long string to "
+                        "require heap allocation"
+                    ))
+                );
                 auto result2 = result1;
 
                 REQUIRE(result1 == result2);
@@ -100,8 +104,9 @@ TEST_CASE("Result combinators and adapters", "[result]") {
             auto result2 = result1.map([](const auto&) { return 2.5; });
 
             REQUIRE(result2 == Ok(2.5));
-            REQUIRE(result2.clone().map(
-                            [](const auto& x) { return x * 2.0; }) == Ok(5.0));
+            REQUIRE(result2.clone().map([](const auto& x) {
+                return x * 2.0;
+            }) == Ok(5.0));
             REQUIRE(result2.clone().map(times2) == Ok(5.0));
             REQUIRE(result2.clone().map(times2_t()) == Ok(5.0));
             REQUIRE(result2.clone().map(std::function(times2_t())) == Ok(5.0));
@@ -109,8 +114,9 @@ TEST_CASE("Result combinators and adapters", "[result]") {
         {
             auto result1 = Result<int, std::string>(Err(std::string("test")));
 
-            REQUIRE(result1.map([](const auto&) { return 5; }) ==
-                    Err(std::string("test")));
+            REQUIRE(result1.map([](const auto&) {
+                return 5;
+            }) == Err(std::string("test")));
         }
     }
     SECTION("map_err") {
@@ -118,18 +124,24 @@ TEST_CASE("Result combinators and adapters", "[result]") {
             auto result1 = Result<int, std::string>(Err(std::string("dog")));
             auto result2 = Result<int, std::string>(Ok(5));
 
-            REQUIRE(result1.map_err([](auto) { return std::string("cat"); }) ==
-                    Err(std::string("cat")));
-            REQUIRE(result2.map_err([](auto) { return std::string("cat"); }) ==
-                    Ok(5));
+            REQUIRE(result1.map_err([](auto) {
+                return std::string("cat");
+            }) == Err(std::string("cat")));
+            REQUIRE(result2.map_err([](auto) {
+                return std::string("cat");
+            }) == Ok(5));
         }
     }
     SECTION("and") {
         {
-            REQUIRE(Result<int, int>(Ok(5)).and_(
-                            Result<double, int>(Ok(2.5))) == Ok(2.5));
-            REQUIRE(Result<int, int>(Err(5)).and_(
-                            Result<double, int>(Ok(2.5))) == Err(5));
+            REQUIRE(
+                Result<int, int>(Ok(5)).and_(Result<double, int>(Ok(2.5)))
+                == Ok(2.5)
+            );
+            REQUIRE(
+                Result<int, int>(Err(5)).and_(Result<double, int>(Ok(2.5)))
+                == Err(5)
+            );
         }
     }
     SECTION("and_then") {
@@ -147,12 +159,17 @@ TEST_CASE("Result combinators and adapters", "[result]") {
     }
     SECTION("or") {
         {
-            REQUIRE(Result<int, int>(Ok(5)).or_(Result<int, int>(Ok(2))) ==
-                    Ok(5));
-            REQUIRE(Result<int, int>(Err(5)).or_(Result<int, double>(Ok(3))) ==
-                    Ok(3));
-            REQUIRE(Result<int, int>(Err(5)).or_(
-                            Result<int, double>(Err(10.0))) == Err(10.0));
+            REQUIRE(
+                Result<int, int>(Ok(5)).or_(Result<int, int>(Ok(2))) == Ok(5)
+            );
+            REQUIRE(
+                Result<int, int>(Err(5)).or_(Result<int, double>(Ok(3)))
+                == Ok(3)
+            );
+            REQUIRE(
+                Result<int, int>(Err(5)).or_(Result<int, double>(Err(10.0)))
+                == Err(10.0)
+            );
         }
     }
     SECTION("or_else") {
@@ -170,10 +187,14 @@ TEST_CASE("Result combinators and adapters", "[result]") {
 TEST_CASE("Hash", "[result]") {
     auto result = Result<int, std::string>(Ok(5));
     const auto result2 = Result<int, std::string>(Err("cat"s));
-    REQUIRE(std::hash<Result<int, std::string>>()(result) ==
-            std::hash<int>()(result.unwrap()));
-    REQUIRE(std::hash<Result<int, std::string>>()(result2) ==
-            std::hash<std::string>()("cat"s));
+    REQUIRE(
+        std::hash<Result<int, std::string>>()(result)
+        == std::hash<int>()(result.unwrap())
+    );
+    REQUIRE(
+        std::hash<Result<int, std::string>>()(result2)
+        == std::hash<std::string>()("cat"s)
+    );
 }
 
 TEST_CASE("Pointer value", "[result]") {
